8d403d09f0579d4538339579eaaaad1157c26f7a
Gavldac <75696834+Gavldac@users.noreply.github.com>
2023-09-30 19:25:48 -0600

    addStore() created

--- SQLTeamProject/src/sqlgroupproject/Store.java
+++ SQLTeamProject/src/sqlgroupproject/Store.java
@@ -20,5 +20,43 @@ public class Store {
 	
 	public static final String dropTable =
 			"DROP TABLE Store";
+
+
+/**
+ * method to add a new store to the Stores database.
+ * 
+ * @param storeID - the manually entered ID number of the new store
+ * @param city - the name of the city of the new store
+ * @param pharmacy - pharmacy is either a 0 or 1 for determining if the store has a pharmacy (0 means no pharm)
+ * @param zip - the zip code for the new store
+ * 
+ * 
+ * @author Edwin
+ */
+	public static void newStore(int storeID, String city, int pharmacy, int zip) {
+		try {
+			Connection connection = DriverManager.getConnection(databaseURL);
+			PreparedStatement prep = connection.prepareStatement(Store.insertData);
+			prep.setInt(1, storeID);
+			prep.setString(2, city);
+			prep.setInt(3, pharmacy);
+			prep.setInt(4, zip);
+
+			int success = prep.executeUpdate();
+
+			if (succeess > 0)
+				System.out.println("New Store added! ");
+			else
+				System.out.println("Bad or incomplete data. Please retry adding new Store.");
+
+			prep.close();
+			connection.close();
+
+		} catch (SQLException e) {
+			System.out.println("There was a problem adding a new store to the Store Database.");
+		}
+	}
+	
+	p
 	
 }
